ca9571b0d68efe5c6aa69d40f6e4202ef8cfd3b6
cogear <cogear@qq.com>
2023-11-25 23:33:18 +0800

    first commit craft

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,50 @@
 <template>
   <v-app>
-    <v-main>
-      <HelloWorld />
+    <v-app-bar flat>
+      <v-container class="mx-auto d-flex align-center justify-center">
+        <v-avatar
+          class="me-4"
+          size="42"
+        >
+          睡前消息
+        </v-avatar>
+
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          :text="link"
+          variant="text"
+        ></v-btn>
+
+        <v-spacer></v-spacer>
+
+        <v-responsive max-width="160">
+          <v-text-field
+            density="compact"
+            label="Search"
+            flat
+            hide-details
+            rounded="1g"
+            single-line
+            variant="solo-filled"
+          ></v-text-field>
+        </v-responsive>
+      </v-container>
+
+
+    </v-app-bar>
+    <v-main class="bg-grey-lighten-3">
+      <ArticleTab />
     </v-main>
   </v-app>
 </template>
 
 <script setup lang="ts">
-  import HelloWorld from '@/components/HelloWorld.vue'
+  import ArticleTab from "@/components/ArticleTab.vue";
+
+  const links = [
+    '文稿',
+    '键政',
+    '新闻',
+  ]
 </script>
---
+++ src/components/ArticleTab.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="2">
+        <v-sheet rounded="1g">
+          <v-list rounded="1g">
+            <v-list-item
+              v-for="n in 673"
+              :key="n"
+              link
+              :title="`睡前消息 ${n}`"
+            >
+            </v-list-item>
+          </v-list>
+        </v-sheet>
+      </v-col>
+
+      <v-col>
+        <v-sheet min-height="70vh">
+
+        </v-sheet>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<style scoped>
+
+</style>
